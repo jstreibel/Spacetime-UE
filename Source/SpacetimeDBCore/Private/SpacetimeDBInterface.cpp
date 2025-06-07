7bab56a537b84b0e9968f87aa8d9534a589eb52a
joao <jstreibel@gmail.com>
2025-06-07 00:06:59 -0300

    Added initial Spacetime interface stub

---
+++ Source/SpacetimeDBCore/Private/SpacetimeDBInterface.cpp
@@ -0,0 +1,10 @@
+#include "SpacetimeDBInterface.h"
+
+#include "ConsoleManager.h"   // for IConsoleManager
+#include "IConsoleManager.h"  // in some engine versions
+#include "Misc/OutputDevice.h"// for GLog
+
+bool USpacetimeDBInterface::IsCLIAvailable()
+{
+	
+}
---
+++ Source/SpacetimeDBCore/Public/SpacetimeDBInterface.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SpacetimeDBInterface.generated.h"
+
+UCLASS(Blueprintable)
+class SPACETIMEDBCORE_API USpacetimeDBInterface : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SpacetimeDB")
+	FString SpacetimeDBModuleName;
+
+	UFUNCTION(BlueprintCallable, Category = "SpacetimeDB")
+	bool IsCLIAvailable();
+	
+	
+};
